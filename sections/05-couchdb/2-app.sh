bbbc252b83d107cf1dc4256f5aed9322794400bf
cristiscu <cristiscu@gmail.com>
2025-03-23 11:05:22 -0700

    couchdb updates

--- sections/05-couchdb/2-app.sh
+++ sections/05-couchdb/2-app.sh
@@ -1,5 +1,7 @@
 curl http://127.0.0.1:5984/
 
+curl http://admin:couchdb@127.0.0.1:5984
+
 curl -X GET http://admin:couchdb@127.0.0.1:5984/_all_dbs
 
 curl -X PUT http://admin:couchdb@127.0.0.1:5984/test2
@@ -11,7 +13,7 @@ curl -X PUT http://admin:couchdb@127.0.0.1:5984/test2/6e1295ed6c29495e54cc05947f
 
 curl -H 'Content-Type: application/json' `
     -X POST http://admin:couchdb@127.0.0.1:5984/test2/_find `
-    -d '{"selector":{"_id":{"$regex": "^6e1.*"}}}'
+    -d '{"selector":{"title":{"$regex": "^There.*"}}}'
 
 curl -X DELETE http://admin:couchdb@127.0.0.1:5984/test2
 
